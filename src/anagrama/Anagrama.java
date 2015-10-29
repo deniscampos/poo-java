5e58879e5243bec3d993b493edd28bae88b16bbe
Denis Campos <deniscpaula@hotmail.com>
2015-10-29 00:40:46 -0200

    Anagrama

---
+++ src/anagrama/Anagrama.java
@@ -0,0 +1,22 @@
+package anagrama;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Anagrama {
+
+	public static boolean comparar(String palavra1, String palavra2) 
+	{
+		palavra1 = palavra1.toLowerCase().replaceAll(" ", "");
+		palavra2 = palavra2.toLowerCase().replaceAll(" ", "");
+		
+		System.out.println(palavra1);
+		List<Character> primeiraLista = new ArrayList<Character>();
+		List<Character> segundaLista = new ArrayList<Character>();
+		
+		for(char c : palavra1.toCharArray())  primeiraLista.add(c);
+		for(char c : palavra2.toCharArray())  segundaLista.add(c);
+		
+		return primeiraLista.containsAll(segundaLista) && primeiraLista.size() == segundaLista.size();
+	}
+}
